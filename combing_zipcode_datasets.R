f46c887f85d21b606d2a6a07dc0ea6f86e4c3322
Shea Zhao <SheaZhao@users.noreply.github.com>
2017-05-08 17:28:45 -0700

    tried some new things
    tried to manipulate data differently to see if it would be easier to combine Chaya's data sets - it didn't work.

--- combing_zipcode_datasets.R
+++ combing_zipcode_datasets.R
@@ -31,25 +31,66 @@ View(AFF$`Percent; HISPANIC OR LATINO - Total population - Hispanic or Latino (o
 head(AFF$Id2) # and this must be the zip_codes
 
 
+# before I change the shape of AFF, let's make a new dataframe with only the variables we care about
+# we selected variables (columns) by index, instead of name, b/c their names are really long & complicated
+AFF2 <- select(AFF, 1,6,7,8,9,10,11,12,13,14,15,16,17,30,31)
+View(AFF2) # has only the 15 variables above
+
+# now I can change the shape of AFF2 to match that of mpv_2
+# these column names are pretty crazy, so I'm going to avoid using them directly
+
+# rename columns so they're easier to work with - order matters!
+names(AFF2) <- c("zip_code","n_white", "freq_white", "n_black", "freq_black", 
+                 "n_native american", "freq_native american", "n_asian", "freq_asian",
+                 "n_pacific islander", "freq_pacific islander", "n_unknown", "freq_unknown",
+                 "n_hispanic", "freq_hispanic")
+View(AFF2)
+
+
+
+
+
+# Now let's look at mpv_2
 unique(mpv_2$race) # reminder of the race categories in mpv_2
 # [1] "White"            "Black"            "Hispanic"         "Native American" 
 # [5] "Asian"            "Unknown race"     "Pacific Islander"
 
 
-
-# Before we can combine these datasets, we need to make the structure & names of rows & columns the same
-    # Notice that in mpv_2 race is a column with subcategories: White, Black, etc.
-    # But in in AFF, each racial category is it's own column (not a subcategory)
-
 # Structure: I'm going to make mpv_2's race subcategories into columns
 View(mpv_2) # before
 
+# since I only care about comparing race & zip code's of CA, I'm excluding everything else
+mpv_R <- select(mpv_2, zip_code, state, race)
+View(mpv_R) # only race & zip_code by state
+mpv_R <- filter(mpv_R, state == "CA") # filtered out all states but CA
+View(mpv_R) 
 
-# exclude using select & then spread & then calculate n & freq
+# we don't need the state column anymore - removing it will make it easire to reshape the data
+mpv_RCA <- select(mpv_R, zip_code, race)
+View(mpv_RCA) # 764 observations
 
 
 
-# since I only care about comparing race & zip code, I excluding everything else
+mpv_RCA.2 <- summarise(mpv_RCA, count = n(), freq = n/sum(n) )
+
+# now I need to spread race so that each race category becomes it's own variable/column
+mpv_RCA.spread <- spread(mpv_RCA, race, zip_code)
+View(mpv_RCA.spread) # frequency of each race per zip code is automatically calculated in the rows
+
+View(AFF)
+
+
+# now I need to spread race so that each race category becomes it's own variable/column
+mpv_R.spread <- spread(mpv_R, zip_code, race)
+View(mpv_R.spread) # frequency of each race per zip code is automatically calculated in the rows
+
+# now that we have freq of each race by zip code, we need the count 
+mpv_R.spread.2 <- mutate(mpv_R.spread, n_white = White/sum(White))
+View(mpv_R.spread.2)
+
+
+
+
 mpv_R <- mpv_2 %>%
     group_by(zip_code, race) %>%
     summarise(n = n(zip_code)) %>%       # I add total counts for race
@@ -66,23 +107,6 @@ U.mpv_R.spread <- summarise(mpv_R.spread,
                             unique_race = n_distinct(race))
 View(U.mpv_R.spread)
 
-
-# before I change the shape of AFF, let's make a new dataframe with only the variables we care about
-# we selected variables (columns) by index, instead of name, b/c their names are really long & complicated
-AFF2 <- select(AFF, 1,6,7,8,9,10,11,12,13,14,15,16,17,30,31)
-View(AFF2) # has only the 15 variables above
-
-# now I can change the shape of AFF2 to match that of mpv_2
-# these column names are pretty crazy, so I'm going to avoid using them directly
-
-# step 1: rename columns so they're easier to work with - order matters!
-names(AFF2) <- c("zip_code","n_white", "freq_white", "n_black", "freq_black", 
-                 "n_native american", "freq_native american", "n_asian", "freq_asian",
-                 "n_pacific islander", "freq_pacific islander", "n_unknown", "freq_unknown",
-                 "n_hispanic", "freq_hispanic")
-View(AFF2)
-
-# step 2: reshape data
 
 
 
