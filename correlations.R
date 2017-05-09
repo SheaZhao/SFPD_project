d2786186ce6d45525b5a91316ab867efcbe3d662
Shea Zhao <SheaZhao@users.noreply.github.com>
2017-05-09 12:16:03 -0700

    plotting race-age correlations

--- correlations.R
+++ correlations.R
@@ -4,15 +4,41 @@
 
 mpv_4.1 # use this dataframe for age
 
+mpv_4.2 <- mpv_4.1 # want to preserve mpv_4.1
+
 # group ages using cut()
-cut_age <- cut(mpv_4.1$age, 
+mpv_4.2$age <- cut(mpv_4.2$age, 
                breaks = c(0, 18, 25, 35, 45, 55, 65, 120), 
                lables = c("0-18 years", "18-25 years", "25-35 years", "35-45 years", "45-55 years", "55-65 years", "66 year or older"),
                right = FALSE)
-levels(cut_age)
-View(cut_age)
+levels(mpv_4.2$age)
+View(mpv_4.2$age)
 
-age_rangesBYrace.box <- ggplot()
+# much broader range for white, black, hispanic levels 
+qplot(data = mpv_4.2, age, fill = age,
+      facets = race~.)
+
+# so will look at them seperatly
+target_WBH <- c("White", "Black", "Hispanic")
+WBH_race_age <- filter(mpv_4.2, race %in% target_WBH )
+unique(WBH_race_age$race) # it worked
+
+target_OTHER <- c("Asian", "Native American", "Pacific Islander", "Unknown race")
+Other_race_age <- filter(mpv_4.2, race %in% target_OTHER)
+unique(Other_race_age$race) # it worked
+
+# now let's plot BWH & Other
+qplot(data = WBH_race_age, age, fill = age,
+      facets = race~.)
+
+qplot(data = Other_race_age, age, fill = age,
+      facets = race~.)
+
+# I should change the death counts to death frequency
+
+
+
+
 
 # deaths by armed/unarmed, fill & facet race
     # box plot for armed categories
