dcaa6be0003bd1bd11cf9d8132d16f342ad057b7
Shea Zhao <SheaZhao@users.noreply.github.com>
2017-05-09 10:43:48 -0700

    plotting correlations
    plotting correlations of deaths by race, age, & COD

---
+++ correlations.R
@@ -0,0 +1,14 @@
+# deaths by age, fill & facet race - cut() into age groups first
+    # box plot for age categories
+    # lm for facets
+
+
+
+# deaths by armed/unarmed, fill & facet race
+    # box plot for armed categories
+    # lm for facets
+
+# COD by race, fill & facet by race
+    # box plot for COD, fill = race
+    # lm for facets
+
