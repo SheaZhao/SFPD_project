fda0710a7c38a79f6ca23d39528b35d52dddc9c3
Shea Zhao <SheaZhao@users.noreply.github.com>
2017-05-09 17:55:51 -0700

    prediction - having trouble
    lm may not be a good model for this data

--- prediction.R
+++ prediction.R
@@ -1,22 +1,64 @@
+# install & load packages
+library(caret)
+install.packages("hydroGOF")
+library(hydroGOF)
+
 # Race & COD correlation
+cor(mpv_2$race, mpv_2$cause_of_death) # can't really use this b/c x must be numeric (y too?)
 
-# Race & age correlation
 
-# Race & gender correlation
+# data split - split rows by (count by 2 on the row indexes)
+colnames(mpv_4.1)
+train_race_all <- mpv_2[seq(1, nrow(mpv_2),2), c("race", "cause_of_death", "charges_brought", "armed_unarmed", "age")]
+test_race_all <- mpv_2[seq(2, nrow(mpv_2),2), c("race", "cause_of_death", "charges_brought", "armed_unarmed", "age")]
 
-# Age & COD correlation - by race?
+# attmenting to remove NA's : Error in na.fail.default(list(race = c("White", "White", "Black", "White",  : 
+# missing values in object
 
-# Age & gender correlation - by race?
+    #na.action=na.omit
 
-# Race & state
+    #> D<-data.frame(x=c(NA,2,3,4,5,6),y=c(2.1,3.2,4.9,5,6,7),residual=NA)
+    #> Z<-lm(y~x,data=D)
+    #> D[names(Z$residuals),"residual"]<-Z$residuals
+    #> D
+   
+    # try taking out charges brought: nope
+        train_race_all <- mpv_2[seq(1, nrow(mpv_2),2), c("race", "cause_of_death", "armed_unarmed", "age")]
+        test_race_all <- mpv_2[seq(2, nrow(mpv_2),2), c("race", "cause_of_death", "armed_unarmed", "age")]
 
-# Race & agency
 
-# State & armed - by race
+     # try changing character variables to factor - maybe?
+    class(mpv_4.1$age) # this is probably ok
+    mpv_4.1$race <- as.factor(mpv_4.1$age)
+    mpv_4.1$race <- as.factor(mpv_4.1$armed_unarmed)
+    mpv_4.1$race <- as.factor(mpv_4.1$cause_of_death)
+    mpv_4.1$race <- as.factor(mpv_4.1$race)
+    class(mpv_4.1$race)
 
-# State & charges - by race
+model_train_all <- train(race~., data = train_race_all, method = "lm")
+    # Error in na.fail.default(list(race = c("White", "White", "Black", "White",  : 
+    # missing values in object
+predictedMPG_all <- predict(mode_train_alll, test_race_all)
+rmse(predictedMPG_all, test_race_all$race)
 
-# State & mental illness - by race
+
+## Race & age correlation ####
+
+## Race & gender correlation ####
+
+## Age & COD correlation - by race? ####
+
+## Age & gender correlation - by race? ####
+
+## Race & state ####
+
+## Race & agency ####
+
+## State & armed - by race ####
+
+## State & charges - by race ####
+
+## State & mental illness - by race ####
 
 data <- realEstate [, c("price", "sq__ft")] # putting my variables into "data"
 
