5c9528012f10f987b2731621a353b20830f4409f
Shea Zhao <SheaZhao@users.noreply.github.com>
2017-05-29 15:15:24 -0700

    new models, first run

--- rule_mining.R
+++ rule_mining.R
@@ -1,16 +1,45 @@
 # tutorial: https://www.slideshare.net/rdatamining/association-rule-mining-with-r
 
+## install & load packages ####
+install.packages("arules")
+install.packages("arulesViz")
+library(arules)
+library(arulesViz)
+library(dplyr)
+
 # new model
 model_4.3.2 <- apriori(spar_trix_4.3, parameter = list(support=0.007, confidence = 0.10, minlen = 2),
-                       contorl = list(verbose = F),
-                       appearance = list(default = 'none', rhs = c(), lhs = c()))
+                       control = list(verbose = F), appearance = list(rhs = c("race"),
+                                         lhs = c("armed_unarmed","cause_of_death",
+                                                 "city", "state", "gender",
+                                                 "mental_illness", "charges_brought")))
+
+summary(model_4.3.2) # set of 30344 rules rules
 
 
-summary(model_4.3.2)
+model_4.3.3 <- apriori(spar_trix_4.3, parameter = list(support=0.007, confidence = 0.10, minlen = 2),
+                       control = list(verbose = F),
+                       appearance = list(rhs = c("armed_unarmed","cause_of_death",
+                                                 "city", "state", "gender",
+                                                 "mental_illness", "charges_brought"),
+                                         lhs = c("race")))
+
+summary(model_4.3.3) # set of 30344 rules
+
+
+model_4.3.4 <- apriori(spar_trix_4.3, parameter = list(support=0.007, confidence = 0.10, minlen = 2),
+                       appearance = list(rhs = c("race")))
+
+summary(model_4.3.4) #set of 30344 rules
+
+
 
 # interpreting rules
-inspect(sort(model_4.3.2, by="lift"))
+inspect(sort(model_4.3.2, by="lift")[1:100])
 
+inspect(sort(model_4.3.3, by="lift")[1:100])
+
+inspect(sort(model_4.3.4, by="lift")[1:100])
 
 # vizualizing rules -- take out all?
 
