ddc68d5fee80b65c25d6c663e07bf81eff9162fb
Shea Zhao <SheaZhao@users.noreply.github.com>
2017-05-29 19:54:24 -0700

    removed redundancies

--- rule_mining2.R
+++ rule_mining2.R
@@ -1,8 +1,6 @@
 # tutorial: https://www.slideshare.net/rdatamining/association-rule-mining-with-r
 
 ## install & load packages ####
-install.packages("arules")
-install.packages("arulesViz")
 library(arules)
 library(arulesViz)
 library(dplyr)
@@ -12,20 +10,6 @@ model_4.3.2 <- apriori(spar_trix_4.3, parameter = list(support=0.007, confidence
 
 summary(model_4.3.2) #set of 30344 rules
 inspect(sort(model_4.3.2, by="lift")[1:100])
-
-# find redundant rules
-subset.matrix <- is.subset(model_4.3.2, model_4.3.2)
-subset.matrix[lower.tri(subset.matrix, diag = T)] <- NA
-redundant <- colSums(subset.matrix, na.rm = T) >= 1
-
-# which rules are redundant - don't run, takes too long
-# which(redundant) # over 1,000 redundant rules
-
-# remove redundant rules
-rules.pruned <- model_4.3.2[!redundant]
-
-# interpreting rules
-inspect(rules.pruned)
 
 # models by race
 model_4.3.4 <- apriori(spar_trix_4.3, control = list(verbose =F),
